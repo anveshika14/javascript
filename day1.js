678d52ba86d8251eb0aeae85393ce3a8a6b5b2ef
anveshika14 <anveshikashukla144@gmail.com>
2025-04-13 22:37:14 +0530

    Exercise for day1

--- day1.js
+++ day1.js
@@ -1,2 +1,68 @@
-// anveshika
-// 14-08-2001
+//Exerice 1
+
+// 1. Write a single line comment which says, comments can make code readable
+// comments can make code readable
+
+// 2. Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
+
+// 3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/*Comments can make code readable, 
+  easy to reuse and
+  informative.*/
+
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types.
+// Declare variables
+   let str="Anveshika"
+   let x=true
+   let y;
+   let n=null
+   console.log(str,x,y,n)
+
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+// Check data types
+   console.log(typeof "Anveshika") 
+   console.log(typeof true) 
+   console.log(typeof y) 
+   console.log(typeof null) 
+   console.log(typeof 2) 
+
+// 6. Declare four variables without assigning values
+// Declare variables
+   let a
+   let b
+   let c
+   let d
+// or
+// let a,b,c,d;
+   console.log(a,b,c,d)
+
+// 7. Declare four variables with assigned values
+// Declare variables
+   let ab=10
+   let cd="Shivanshi"
+   let ef=true
+   let gh=null
+   console.log(ab,cd,ef,gh)
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+// Declare variables
+   let fName="Anveshika"
+   let lName="Shivanshi"
+   let maritalStatus="Single"
+   let country="India"
+   let age=23
+   console.log(fName,lName,maritalStatus,country,age)
+
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+// Declare variables
+   let firstName="Anveshika",lastName="Shivanshi",mStatus="Single",my_country="India",my_age=23
+   console.log(firstName,lastName,maritalStatus,country,age)
+
+// 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+// Declare variables
+   let myAge = 25;
+   let yourAge = 30;
+
+   console.log("I am " + myAge + " years old.");
+   console.log("You are " + yourAge + " years old.");
