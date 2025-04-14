2ea1d16ec5bf0859a9c565785604623838fef0f5
anveshika14 <anveshikashukla144@gmail.com>
2025-04-14 22:48:58 +0530

    Que1and 2

---
+++ day2/Exercise1.js
@@ -0,0 +1,7 @@
+// 1. Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'
+// Declare variable
+   let challenge = '30 Days Of JavaScript'
+
+// 2. Print the string on the browser console using console.log()
+   console.log(challenge)
+
