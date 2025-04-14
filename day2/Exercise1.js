06ee209b5cdcaf2013ebce2a4367985e3af59ffd
anveshika14 <anveshikashukla144@gmail.com>
2025-04-14 22:50:23 +0530

    que3

--- day2/Exercise1.js
+++ day2/Exercise1.js
@@ -5,3 +5,5 @@
 // 2. Print the string on the browser console using console.log()
    console.log(challenge)
 
+// 3. Print the length of the string on the browser console using console.log()
+   console.log(challenge.length)
