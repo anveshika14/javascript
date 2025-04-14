fdc52aebef64d7304bf3e5b4df478869e17d8869
anveshika14 <anveshikashukla144@gmail.com>
2025-04-14 22:57:32 +0530

    html added

--- day2/Exercise1.js
+++ day2/Exercise1.js
@@ -7,3 +7,24 @@
 
 // 3. Print the length of the string on the browser console using console.log()
    console.log(challenge.length)
+
+// 4. Change all the string characters to capital letters using toUpperCase() method
+   console.log(challenge.toUpperCase())
+
+// 5. Change all the string characters to lowercase letters using toLowerCase() method
+    console.log(challenge.toLowerCase())
+
+// 6. Cut (slice) out the first word of the string using substr() or substring() method
+    console.log(challenge.substring(3))
+
+// 7. Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+    console.log(challenge.slice(3, 21))
+
+// 8. Check if the string contains a word Script using includes() method
+    console.log(challenge.includes("Script"))
+
+// 9. Split the string into an array using split() method
+    console.log(challenge.split())
+
+// 10. Split the string 30 Days Of JavaScript at the space using split() method
+    console.log(challenge.split(" "))
