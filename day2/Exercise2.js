7460224db47ec1621de1aa02326fe1f090356a2f
anveshika14 <anveshikashukla144@gmail.com>
2025-04-15 22:29:51 +0530

    exercise level 2 added

---
+++ day2/Exercise2.js
@@ -0,0 +1,24 @@
+// 1. Using console.log() print out the following statement:
+// The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+   console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
+
+// 2. Using console.log() print out the following quote by Mother Teresa:
+// "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+   console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
+
+// 3. Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+    console.log(typeof '10' === 10)  
+    console.log(typeof '10' == 10) 
+
+// 4. Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+    console.log(parseFloat('9.8') === 10)  
+    console.log(parseFloat('9.8') == 10)
+
+// 5. Check if 'on' is found in both python and jargon
+    let str1 = "python"
+    let str2 = "jargon"
+    console.log(str1.includes("on") && str2.includes("on")) 
+
+// 6. I hope this course is not full of jargon. Check if jargon is in the sentence.
+    let sentence = "I hope this course is not full of jargon."
+    console.log(sentence.includes("jargon"))
