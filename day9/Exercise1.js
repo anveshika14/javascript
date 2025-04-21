27dab3e9a98eacf06a0523b94d8f8cb3e4ae2c03
anveshika14 <anveshikashukla144@gmail.com>
2025-04-21 21:42:43 +0530

    day 9 started

---
+++ day9/Exercise1.js
@@ -0,0 +1,7 @@
+//1. Use forEach to console.log each country in the countries array.
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland'];
+countries.forEach(country => console.log(country));
+
+//2. Use forEach to console.log each name in the names array.
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook'];
+names.forEach(name => console.log(name));
