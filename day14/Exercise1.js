3819b6f76ebf33a2cce7a3721df61ebc125ef732
anveshika14 <anveshikashukla144@gmail.com>
2025-04-27 23:29:56 +0530

    exercise 1 done

--- day14/Exercise1.js
+++ day14/Exercise1.js
@@ -11,6 +11,17 @@ try {
   let lastName = 'Yetayeh'
   let fullName = fistName + ' ' + lastName
 } catch (err) {
+  console.error(err) // we can use console.log() or console.error()
+} finally {
+  console.log('In any case I will be executed')
+}
+
+// Output: ReferenceError: fistName is not defined
+
+try {
+  let lastName = 'Yetayeh'
+  let fullName = fistName + ' ' + lastName
+} catch (err) {
   console.log('Name of the error', err.name)
   console.log('Error message', err.message)
 } finally {
