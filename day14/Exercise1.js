08eab2759bdea0b5bfee0bf21aece69b39f69f56
anveshika14 <anveshikashukla144@gmail.com>
2025-04-27 23:29:09 +0530

    try and catch 1

--- day14/Exercise1.js
+++ day14/Exercise1.js
@@ -7,3 +7,16 @@ try {
 
 // Output: ReferenceError: fistName is not defined
 
+try {
+  let lastName = 'Yetayeh'
+  let fullName = fistName + ' ' + lastName
+} catch (err) {
+  console.log('Name of the error', err.name)
+  console.log('Error message', err.message)
+} finally {
+  console.log('In any case I will be executed')
+}
+
+// Output: Name of the error ReferenceError
+
+
