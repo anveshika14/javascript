0874ae6f88fdab6980dcadab72648897aaed98a5
anveshika14 <anveshikashukla144@gmail.com>
2025-04-27 23:34:59 +0530

    reference error

---
+++ day14/Exercise3.js
@@ -0,0 +1,6 @@
+// Error Types
+// 1-ReferenceError: An illegal reference has occurred. A ReferenceError is thrown if we use a variable that has not been declared.
+let firstName = 'Anveshika'
+let fullName = firstName + ' ' + lastName
+
+console.log(fullName)
