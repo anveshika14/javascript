2cf51aa14956deecba47170ced4a3071294a1089
anveshika14 <anveshikashukla144@gmail.com>
2025-04-27 23:36:06 +0530

    syntax error

--- day14/Exercise3.js
+++ day14/Exercise3.js
@@ -2,5 +2,9 @@
 // 1-ReferenceError: An illegal reference has occurred. A ReferenceError is thrown if we use a variable that has not been declared.
 let firstName = 'Anveshika'
 let fullName = firstName + ' ' + lastName
-
 console.log(fullName)
+
+// 2-SyntaxError: A syntax error has occurred
+let square = 2 * 2
+console.log(square)
+console.log('Hello, world')
