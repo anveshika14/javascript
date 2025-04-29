5d88ca699709427c15764931165f61a783f50b5f
anveshika14 <anveshikashukla144@gmail.com>
2025-04-29 22:17:03 +0530

    que3

--- day16/Exercise1.js
+++ day16/Exercise1.js
@@ -8,3 +8,7 @@ const age = 25;
 const ageJSON = JSON.stringify(age);
 console.log(ageJSON);
 
+//3.Stringify the isMarried variable
+const isMarried = false;
+const isMarriedJSON = JSON.stringify(isMarried);
+console.log(isMarriedJSON);
