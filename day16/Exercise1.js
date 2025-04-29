d344d5d4d579e0c521a14e017569e228535e7dd1
anveshika14 <anveshikashukla144@gmail.com>
2025-04-29 22:15:44 +0530

    que1

---
+++ day16/Exercise1.js
@@ -0,0 +1,5 @@
+//1.Change skills array to JSON using JSON.stringify()
+const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
+const skillsJSON = JSON.stringify(skills);
+console.log(skillsJSON); 
+
