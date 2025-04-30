f41121172c4cee564ffa79e4400b28a39a85d963
anveshika14 <anveshikashukla144@gmail.com>
2025-04-30 23:24:48 +0530

    exercise 1 done

---
+++ day17/Exercise1.js
@@ -0,0 +1,8 @@
+//1.Store you first name, last name, age, country, city in your browser localStorage.
+
+localStorage.setItem("firstName", "Anveshika");
+localStorage.setItem("lastName", "Shukla");
+localStorage.setItem("age", "23");
+localStorage.setItem("country", "India");
+localStorage.setItem("city", "Lucknow");
+
