c72a48d343f6fa2280dab3919193cc0f5c97cc27
anveshika14 <anveshikashukla144@gmail.com>
2025-05-02 22:27:46 +0530

    exercise 3 done

--- day19/Exercise3.js
+++ day19/Exercise3.js
@@ -1,6 +1,4 @@
 //1.Create a personAccount out function. It has firstname, lastname, incomes, expenses inner variables. It has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance inner functions. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
-
-
 function personAccount() {
     let firstName = "Anveshika";
     let lastName = "Shukla";
