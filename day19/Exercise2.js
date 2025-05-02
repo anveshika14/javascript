7cbbfbd2d3a1dd10e7de03135209121da65b855b
anveshika14 <anveshikashukla144@gmail.com>
2025-05-02 22:24:38 +0530

    exercise 2 done

--- day19/Exercise2.js
+++ day19/Exercise2.js
@@ -1,1 +1,23 @@
 //1.Create a closure which has three inner functions
+function outerFunction() {
+    let outerVariable = "I am an outer variable";
+
+    function innerFunction1() {
+        console.log(outerVariable + " from inner function 1");
+    }
+
+    function innerFunction2() {
+        console.log(outerVariable + " from inner function 2");
+    }
+
+    function innerFunction3() {
+        console.log(outerVariable + " from inner function 3");
+    }
+
+    return [innerFunction1, innerFunction2, innerFunction3];
+}
+
+const [closureFunction1, closureFunction2, closureFunction3] = outerFunction();
+closureFunction1(); 
+closureFunction2();
+closureFunction3(); 
